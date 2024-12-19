8194266baffc05924ca13bc28b4eda894948bb8d
fedorka3 <35789@ocvts.org>
2024-12-19 14:58:55 +0000

    movement

--- js/game.js
+++ js/game.js
@@ -3,6 +3,13 @@
 let canvas;
 let ctx;
 
+//Create input variables
+
+let upKey;
+let rightKey;
+let downKey;
+let leftKey;
+
 let gameLoop;
 let player;
 
@@ -13,6 +20,7 @@ window.onload = function() {
     ctx = canvas.getContext("2d");
 
     //Setup key listeners
+    setupInputs();
 
     //Create player
     player = new Player(100, 400);
@@ -38,4 +46,30 @@ function draw() {
 
     //Draw the player
     player.draw();
+}
+
+function setupInputs() {
+    document.addEventListener("keydown", function(event) {
+        if (event.key === '"w"' || event.key === "ArrowUp") {
+            upKey = true;
+        } else if (event.key === "a" || event.key === "ArrowLeft") {
+            leftKey = true;
+        } else if (event.key === "s" || event.key === "ArrowDown") {
+            downKey = true;
+        } else if (event.key === "d" || event.key === "ArrowRight") {
+            rightKey = true;
+        }
+    });
+
+    document.addEventListener("keyup", function(event) {
+        if (event.key === '"w"' || event.key === "ArrowUp") {
+            upKey = false;
+        } else if (event.key === "a" || event.key === "ArrowLeft") {
+            leftKey = false;
+        } else if (event.key === "s" || event.key === "ArrowDown") {
+            downKey = false;
+        } else if (event.key === "d" || event.key === "ArrowRight") {
+            rightKey = false;
+        }
+    });
 }
--- js/player.js
+++ js/player.js
@@ -1,12 +1,47 @@
 function Player(x, y) {
+    //Player variables
     this.x = x;
     this.y = y;
     
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.friction = 0.6;
+    this.maxSpeed = 18;
+    
     this.width = 50;
     this.height = 55;
+
+    this.active = true;
     
     this.step = function() {
-        this.x ++
+      //Movement
+      if (this.active) {
+        //Horizontal Movement
+        if (!leftKey && !rightKey || leftKey && rightKey) {
+            //Slow down
+            this.xSpeed *= this.friction
+        } else if (rightKey) {
+            this.xSpeed ++;
+        } else if (leftKey) {
+            //Move left
+            this.xSpeed --;
+        }
+        
+        //Vertical Movement
+        
+
+        //Apply gravity
+
+        //Correct Speed
+        if (this.xSpeed > this.maxSpeed) {
+            this.xSpeed = this.maxSpeed;
+        } else if (this.xSpeed < -this.maxSpeed) {
+            this.xSpeed = -this.maxSpeed;
+        }
+
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+      }
     }
 
     this.draw = function() {
